c61ca2d29e0ce802ee60d3fbcb94cc0ca1fab910
lyonish <singing.a.song.sung.in.the.sun@gmail.com>
2018-04-05 15:33:58 +0900

    WIP before changing views directory due to wrapper webcomponent

--- src/components/blog-container/blog-article/blog-article.js
+++ src/components/blog-container/blog-article/blog-article.js
@@ -1,8 +1,57 @@
-class BlogArticle extends HTMLElement {
 
-  constructor(){
-    super();
+class BlogArticle extends HTMLElement {
+  get title() {
+    if (this.hasAttribute('title')){
+      return this.attributes.title.value;
+    }
+    return null;
   }
 
+  set title(val) {
+    if (val) {
+      this.setAttribute('title', val);
+    } 
+  }
 
+  static get observedAttributes() {
+    return [''];
+  }
+
+  constructor(data) {
+    super();
+    let shadowRoot = this.attachShadow({mode: 'open'});
+    const template = document.querySelector('#blog-article');
+    const instance = template.content.cloneNode(true);
+
+    shadowRoot.appendChild(instance);
+    this.title = data.title;
+    this.postDate = data.postDate;
+    this.author = data.author;
+    this.content = data.content;
+    shadowRoot.querySelector("h3").textContent = this.title;
+    shadowRoot.querySelector(".postdate").textContent = this.postDate;
+    shadowRoot.querySelector(".author").textContent = this.author;
+    shadowRoot.querySelector(".content").textContent = this.content;
+
+    this.componentId = window.document.nextComponentSerial;
+    history.state[this.componentId] = {componentName: this.nodeName};
+    window.document.nextComponentSerial = this.componentId + 1;
+
+    // history.state[this.componentId].count = value;
+    window.addEventListener('popstate', e => {
+      // this.value = history.state[this.componentId].count;
+    });
+    this.addEventListener('click', e => {
+
+    });
+    // shadowRoot.querySelector('li').addEventListener('click', (e)=> {
+    // })
+  }
+  // Respond to attribute changes.
+  attributeChangedCallback(attr, oldValue, newValue) {
+    // if (attr == 'value') {
+    // this.shadowRoot.querySelector('span').textContent = newValue;
+    // }
+  }
 }
+customElements.define('blog-article', BlogArticle);
---
+++ src/components/blog-container/blog-container.js
@@ -0,0 +1,85 @@
+
+class BlogContainer extends HTMLElement {
+  // A getter/setter for an open property.
+  get open() {
+    return this.hasAttribute('open');
+  }
+
+  set open(val) {
+    // Reflect the value of the open property as an HTML attribute.
+    if (val) {
+      this.setAttribute('open', '');
+    } else {
+      this.removeAttribute('open');
+    }
+    // this.toggleDrawer();
+  }
+
+  // A getter/setter for a disabled property.
+  get disabled() {
+    return this.hasAttribute('disabled');
+  }
+
+  set disabled(val) {
+    // Reflect the value of the disabled property as an HTML attribute.
+    if (val) {
+      this.setAttribute('disabled', '');
+    } else {
+      this.removeAttribute('disabled');
+    }
+  }
+
+  get src() {
+    if (this.hasAttribute('src')){
+      return this.attributes.src.value;
+    }
+    return null;
+  }
+
+  // Can define constructor arguments if you wish.
+  constructor() {
+    // If you define a ctor, always call super() first!
+    // This is specific to CE and required by the spec.
+    super();
+    let shadowRoot = this.attachShadow({mode: 'open'});
+    const template = document.querySelector('#blog-container');
+    const instance = template.content.cloneNode(true);
+    shadowRoot.appendChild(instance);
+
+    this.componentId = window.document.nextComponentSerial;
+    history.state[this.componentId] = {componentName: this.nodeName};
+    window.document.nextComponentSerial = this.componentId + 1;
+
+    // this.addEventListener('click', e => {
+    //   if (this.disabled) {
+    //     return false;
+    //   }
+    // });
+  }
+
+  connectedCallback() {
+    const _shadowRoot = this.shadowRoot;
+    let instance;
+    fetch(this.src)
+    .then(function(response) {
+      return response.json().then(function(json) {
+        for (let i=0; i <json.length; i = i+1){
+          instance = new BlogArticle(json[i]);
+          _shadowRoot.querySelector('section').appendChild(instance);
+      }
+    });
+  });
+}
+
+  // Respond to attribute changes.
+  attributeChangedCallback(attr, oldValue, newValue) {
+    if (attr == 'src') {
+      console.log('blog-container  -  attributeChangedCallback')
+    }
+  }
+
+  // toggleDrawer() {
+  //   alert('mycompo!');
+  // }
+}
+customElements.define('blog-container', BlogContainer);
